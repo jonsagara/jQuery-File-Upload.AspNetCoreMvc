e1bbbe6cf2fa48d61e2fe3f0a60d380f5cc351f1
Jon Sagara <jon@sagarasoftware.com>
2017-06-02 09:24:41 -0700

    Moved the file upload hookup code into index.js.

--- src/jQuery-File-Upload.AspNetCoreMvc/wwwroot/js/fileupload/index.js
+++ src/jQuery-File-Upload.AspNetCoreMvc/wwwroot/js/fileupload/index.js
@@ -1,1 +1,13 @@
-﻿
+﻿// Write your Javascript code.
+
+
+var $form = null;
+$(function () {
+
+    $form = $('#fileupload').fileupload({
+        dataType: 'json'
+    });
+
+});
+
+//$('#fileupload').addClass('fileupload-processing');
