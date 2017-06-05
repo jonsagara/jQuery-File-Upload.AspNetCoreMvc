7d1a7b9f40d852b0f4ceddbddfb8b74dd212b44a
Jon Sagara <jon@sagarasoftware.com>
2017-06-05 11:18:14 -0700

    Saving work, but abandoning SkiaSharp because Skia doesn't support writing GIFs.

--- src/jQuery-File-Upload.AspNetCoreMvc/Models/FileUpload/FileUploadUpload.cs
+++ src/jQuery-File-Upload.AspNetCoreMvc/Models/FileUpload/FileUploadUpload.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using jQuery_File_Upload.AspNetCoreMvc.Utilities;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using SkiaSharp;
 
 namespace jQuery_File_Upload.AspNetCoreMvc.Models.FileUpload
 {
@@ -50,8 +51,11 @@ namespace jQuery_File_Upload.AspNetCoreMvc.Models.FileUpload
 
             private async Task UploadWholeFileAsync(Command message, CommandResult result)
             {
-                // Ensure the storage root exists.
+                const string THUMBS = "thumbs";
+
+                // Ensure the storage root and thumbnail directories exist.
                 Directory.CreateDirectory(_filesHelper.StorageRootPath);
+                Directory.CreateDirectory(Path.Combine(_filesHelper.StorageRootPath, THUMBS));
 
                 foreach (var file in message.Files)
                 {
@@ -62,33 +66,90 @@ namespace jQuery_File_Upload.AspNetCoreMvc.Models.FileUpload
                         {
                             await file.CopyToAsync(fs);
                         }
+
+                        // Create thumbnail: 80x80 for sure, and if the image is > 540x360, create one that's limited to
+                        //   either 540 wide or 360 tall, depending on which dimension is larger.
+                        var extension = Path.GetExtension(fullPath).ToLower();
+                        if (extension != ".jpg" && extension != ".jpeg" && extension != "png" && extension != ".gif")
+                        {
+                            // Not a supported image type; don't resize.
+                            continue;
+                        }
+
+
+                        const int THUMB_WIDTH = 80;
+                        const int THUMB_HEIGHT = 80;
+                        const int REGULAR_WIDTH = 540;
+                        //const int REGULAR_HEIGHT = 360;
+
+                        // Create the 80x80 thumbnail first.
+                        using (var input = File.OpenRead(fullPath))
+                        using (var inputStream = new SKManagedStream(input))
+                        using (var original = SKBitmap.Decode(inputStream))
+                        {
+                            using (var resized = original.Resize(new SKImageInfo(THUMB_WIDTH, THUMB_HEIGHT), SKBitmapResizeMethod.Lanczos3))
+                            {
+                                if (resized == null)
+                                {
+                                    throw new Exception($"Unable to create {THUMB_WIDTH}x{THUMB_HEIGHT} thumbnail image for {fullPath}");
+                                }
+
+                                var fileNameWithoutExt = Path.GetFileNameWithoutExtension(file.FileName);
+                                var outputPath = Path.Combine(_filesHelper.StorageRootPath, THUMBS, $"{fileNameWithoutExt}{THUMB_WIDTH}x{THUMB_HEIGHT}{extension}");
+
+                                SKEncodedImageFormat imageFormat = SKEncodedImageFormat.Jpeg;
+                                switch(extension)
+                                {
+                                    case ".jpeg":
+                                    case ".jpg":
+                                        imageFormat = SKEncodedImageFormat.Jpeg;
+                                        break;
+
+                                    case ".png":
+                                        imageFormat = SKEncodedImageFormat.Png;
+                                        break;
+
+                                    case ".gif":
+                                        imageFormat = SKEncodedImageFormat.Gif;
+                                        break;
+                                }
+
+                                using (var image = SKImage.FromBitmap(resized))
+                                using (var output = File.OpenWrite(outputPath))
+                                {
+                                    image
+                                        .Encode(imageFormat, quality: 75)
+                                        .SaveTo(output);
+                                }
+                            }
+                        }
                     }
 
-                    ////Create thumb
-                    //string[] imageArray = file.FileName.Split('.');
-                    //if (imageArray.Length != 0)
-                    //{
-                    //    string extansion = imageArray[imageArray.Length - 1].ToLower();
-                    //    if (extansion != "jpg" && extansion != "png" && extansion != "jpeg") //Do not create thumb if file is not an image
-                    //    {
+                        ////Create thumb
+                        //string[] imageArray = file.FileName.Split('.');
+                        //if (imageArray.Length != 0)
+                        //{
+                        //    string extansion = imageArray[imageArray.Length - 1].ToLower();
+                        //    if (extansion != "jpg" && extansion != "png" && extansion != "jpeg") //Do not create thumb if file is not an image
+                        //    {
 
-                    //    }
-                    //    else
-                    //    {
-                    //        var ThumbfullPath = Path.Combine(pathOnServer, "thumbs");
-                    //        //string fileThumb = file.FileName + ".80x80.jpg";
-                    //        string fileThumb = Path.GetFileNameWithoutExtension(file.FileName) + "80x80.jpg";
-                    //        var ThumbfullPath2 = Path.Combine(ThumbfullPath, fileThumb);
-                    //        using (MemoryStream stream = new MemoryStream(File.ReadAllBytes(fullPath)))
-                    //        {
-                    //            var thumbnail = new WebImage(stream).Resize(80, 80);
-                    //            thumbnail.Save(ThumbfullPath2, "jpg");
-                    //        }
+                        //    }
+                        //    else
+                        //    {
+                        //        var ThumbfullPath = Path.Combine(pathOnServer, "thumbs");
+                        //        //string fileThumb = file.FileName + ".80x80.jpg";
+                        //        string fileThumb = Path.GetFileNameWithoutExtension(file.FileName) + "80x80.jpg";
+                        //        var ThumbfullPath2 = Path.Combine(ThumbfullPath, fileThumb);
+                        //        using (MemoryStream stream = new MemoryStream(File.ReadAllBytes(fullPath)))
+                        //        {
+                        //            var thumbnail = new WebImage(stream).Resize(80, 80);
+                        //            thumbnail.Save(ThumbfullPath2, "jpg");
+                        //        }
 
-                    //    }
-                    //}
+                        //    }
+                        //}
 
-                    result.FileResults.Add(UploadResult(file.FileName, file.Length));
+                        result.FileResults.Add(UploadResult(file.FileName, file.Length));
                 }
             }
 
